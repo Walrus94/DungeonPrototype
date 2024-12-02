113b79f9dbb15268945de14f0d580169698a417d
Walrus94 <arsnazarov94@gmail.com>
2024-12-02 13:58:17 +0100

    main web app config fix

--- src/main/java/org/dungeon/prototype/config/SecurityConfig.java
+++ src/main/java/org/dungeon/prototype/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,7 +14,6 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Value("${bot.path}")
