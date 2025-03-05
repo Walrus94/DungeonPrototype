174a471011a75c445eedf26ec57ff85df877a7aa
Walrus94 <arsnazarov94@gmail.com>
2025-03-05 15:24:32 +0100

    Fixed postgres default user initialization

--- init-scripts/init-multiple-dbs.sh
+++ init-scripts/init-multiple-dbs.sh
@@ -3,9 +3,12 @@ set -e
 
 # Use the PostgreSQL user from environment variables
 POSTGRES_USER="${POSTGRES_DATABASE_USER:-postgres}"
+POSTGRES_PASSWORD="${POSTGRES_DATABASE_PASSWORD:-}"
 
 DATABASES=("balance_matrices_dev" "balance_matrices_prod" "balance_matrices_test")
 
+CREATE ROLE "$POSTGRES_USER" WITH LOGIN PASSWORD "$POSTGRES_PASSWORD";
+ALTER ROLE "$POSTGRES_USER" CREATEDB;
 for db in "${DATABASES[@]}"; do
   echo "Creating database: $db"
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
