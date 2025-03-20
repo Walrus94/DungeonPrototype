de1c36c57fd9537d7eedd871eebe888111dd0417
Walrus94 <arsnazarov94@gmail.com>
2025-03-20 12:15:50 +0100

    fixed balance matrix topic naming

--- huggingface-api/kafka/consumer.py
+++ huggingface-api/kafka/consumer.py
@@ -1,6 +1,6 @@
 import logging
 from confluent_kafka import Consumer
-from config.settings import KAFKA_BOOTSTRAP_SERVER, KAFKA_TOPIC_BALANCES, KAFKA_TOPIC_ITEM_NAMING
+from config.settings import KAFKA_BOOTSTRAP_SERVER, KAFKA_BALANCE_MATRIX_TOPIC, KAFKA_TOPIC_ITEM_NAMING
 from kafka.process_balance import process_kafka_balance_message
 from kafka.process_item_naming import process_kafka_item_message
 
@@ -13,7 +13,7 @@ kafka_consumer = Consumer({
     'auto.offset.reset': 'earliest'
 })
 
-kafka_consumer.subscribe([KAFKA_TOPIC_BALANCES, KAFKA_TOPIC_ITEM_NAMING])
+kafka_consumer.subscribe([KAFKA_BALANCE_MATRIX_TOPIC, KAFKA_TOPIC_ITEM_NAMING])
 
 def consume_messages():
     """Listens for balance matrix requests."""
@@ -29,7 +29,7 @@ def consume_messages():
         topic = msg.topic()
         if topic == KAFKA_TOPIC_ITEM_NAMING:
             process_kafka_item_message(msg.value().decode('utf-8'))
-        elif topic == KAFKA_TOPIC_BALANCES:
+        elif topic == KAFKA_BALANCE_MATRIX_TOPIC:
             process_kafka_balance_message(msg.value().decode('utf-8'))
 
 if __name__ == '__main__':
