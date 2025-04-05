41ed98b25157b257e1b4eb10e333b7a9bf4bb7f2
Walrus94 <arsnazarov94@gmail.com>
2025-04-05 10:42:05 +0200

    secured mongo monitoring user

--- init-mongo.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB("admin");
-db.createUser({
-    user: "monitoring",
-    pwd: "monitoringpassword",
-    roles: [
-        { role: "clusterMonitor", db: "admin" },
-        { role: "read", db: "local" }
-    ]
-});
---
+++ init-scripts/init-mongo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Initializing MongoDB with environment variables..."
+
+# Run MongoDB commands using the environment variables
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+db = db.getSiblingDB("admin");
+db.createUser({
+  user: "MONGO_MONITORING_USER",
+  pwd: "MONGO_MONITORING_PASSWORD",
+  roles: [
+          { role: "clusterMonitor", db: "admin" },
+          { role: "read", db: "local" }
+      ]
+});
+EOF
