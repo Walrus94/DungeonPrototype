b1534051989d429ac7e67b467a751d75aff16845
Walrus94 <arsnazarov94@gmail.com>
2025-05-25 23:14:59 +0200

    reverted latest fix

--- src/main/java/org/dungeon/prototype/repository/postgres/BalanceMatrixRepository.java
+++ src/main/java/org/dungeon/prototype/repository/postgres/BalanceMatrixRepository.java
@@ -4,9 +4,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.Array;
-import java.sql.SQLException;
-
 
 @Service
 public class BalanceMatrixRepository {
@@ -27,32 +24,15 @@ public class BalanceMatrixRepository {
     }
 
     public float getValue(Long chatId, String name, int row, int col) {
-        String sql =
-                String.format("SELECT data[%d][%d] FROM matrices WHERE chat_id = ? AND name = ?",
-                        row, col);
+        String sql = String.format("SELECT (data[%d][%d])::double precision AS cell FROM matrices WHERE chat_id = ? AND name = ?", row, col);
         return jdbcTemplate.query(sql, ps -> {
             ps.setLong(1, chatId);
             ps.setString(2, name);
         }, rs -> {
-            if (!rs.next()) {
-                throw new EmptyResultDataAccessException(1);
-            }
-
-            Array array = rs.getArray("data");
-            if (array == null) {
-                throw new SQLException("Array column is null");
-            }
-
-            Object raw = array.getArray();
-            if (!(raw instanceof Object[][] obj2D)) {
-                throw new SQLException("Expected 2D array but got: " + raw.getClass());
-            }
-
-            Object cell = obj2D[row][col];
-            if (cell instanceof Number number) {
-                return number.floatValue();
+            if (rs.next()) {
+                return rs.getFloat("cell");
             } else {
-                throw new SQLException("Array element is not a number: " + cell);
+                throw new EmptyResultDataAccessException(1);
             }
         });
     }
