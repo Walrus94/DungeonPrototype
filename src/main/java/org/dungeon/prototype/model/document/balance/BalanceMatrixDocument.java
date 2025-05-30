ee09af622f89fca5d5011441dad765d459dcfa9c
Walrus94 <arsnazarov94@gmail.com>
2025-05-30 08:25:26 +0200

    fixed balance matrices indexing

--- src/main/java/org/dungeon/prototype/model/document/balance/BalanceMatrixDocument.java
+++ src/main/java/org/dungeon/prototype/model/document/balance/BalanceMatrixDocument.java
@@ -3,18 +3,18 @@ package org.dungeon.prototype.model.document.balance;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
 @Document(collection = "balance_matrices")
+@CompoundIndex(name = "chatId_name_idx", def = "{'chatId': 1, 'name': 1}", unique = true)
 @NoArgsConstructor
 public class BalanceMatrixDocument {
     @Id
     private String id;
-    @Indexed
     private Long chatId;
     private String name;
     private List<List<Double>> data;
