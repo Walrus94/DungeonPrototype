58639661941a78900b12b09056cf88a5c1e35e0f
Walrus94 <arsnazarov94@gmail.com>
2025-05-31 10:57:44 +0200

    test commit

--- src/main/java/org/dungeon/prototype/repository/mongo/BalanceMatrixRepository.java
+++ src/main/java/org/dungeon/prototype/repository/mongo/BalanceMatrixRepository.java
@@ -4,16 +4,18 @@ import org.dungeon.prototype.model.document.balance.BalanceMatrixDocument;
 import org.dungeon.prototype.repository.mongo.projections.MatrixValueProjection;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BalanceMatrixRepository extends MongoRepository<BalanceMatrixDocument, String> {
     boolean existsByChatIdAndName(long chatId, String name);
     BalanceMatrixDocument findByChatIdAndName(long chatId, String name);
     @Aggregation(pipeline = {
             "{ '$match': { 'chatId': ?0, 'name': ?1 } }",
-            "{ '$project': { 'data': { '$arrayElemAt': ['$data', ?2] } } }",
-            "{ '$project': { 'value': { '$arrayElemAt': ['$data', ?3] } } }"
+            "{ '$project': { 'data': { '$arrayElemAt': ['$data', ?3] } } }",
+            "{ '$project': { 'value': { '$arrayElemAt': ['$data', ?2] } } }"
     })
     Optional<MatrixValueProjection> getMatrixCellValue(Long chatId, String name, int row, int col);
     void deleteAllByChatId(long chatId);
